ad662ecc077abdc91fe8f444d1b45f32fec4cb84
Will Medina <williams.medinaa@gmail.com>
2026-02-06 08:00:36 -0700

    chore: remove alpha

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,18 +1,64 @@
 return {
   {
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp' },
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',       
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-buffer',             
+      'hrsh7th/cmp-path',             
+    },
     config = function()
       local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
       cmp.setup({
-        sources = {
-          { name = 'nvim-lsp' },        
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
         },
+        
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+
         mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(), -- Manual trigger if needed
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' }, -- Primary source (Biome, ts_ls, etc.)
+          { name = 'luasnip' },  -- For code snippets
+        }, {
+          { name = 'buffer' },   -- Text from current file
+          { name = 'path' },     -- File system paths
         }),
       })
     end
-  }}
+  }
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,44 +26,5 @@ return {
       }
     }
   },
-  {
-    "goolord/alpha-nvim",
-    lazy = false,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local alpha = require("alpha")
-      local dashboard = require("alpha.themes.dashboard")
-
-      -- 1. The Logo (Header)
-      dashboard.section.header.val = {
-        [[                               __                ]],
-        [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-        [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-        [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-        [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-        [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-      }
-
-      -- 2. The Buttons (Shortcuts)
-      dashboard.section.buttons.val = {
-        dashboard.button("f", "  Find File", ":Telescope find_files <CR>"),
-        dashboard.button("p", "  Recent Projects", ":Telescope projects <CR>"),
-        dashboard.button("r", "  Recent Files", ":Telescope oldfiles <CR>"),
-        dashboard.button("n", "  New File", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("c", "  Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("q", "  Quit", ":qa<CR>"),
-      }
-
-      -- 3. The Footer (Project Context)
-      local stats = vim.uv.uptime()
-      dashboard.section.footer.val = "Nvim loaded in " .. stats .. "ms"
-
-      -- 4. Apply Highlights
-      dashboard.section.header.opts.hl = "Function"
-      dashboard.section.buttons.opts.hl = "Identifier"
-      dashboard.section.footer.opts.hl = "Comment"
-
-      alpha.setup(dashboard.config)
-    end,
-  },
+  
 }
