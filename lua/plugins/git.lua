de479adbd75f00d5981d7bdcf3d21db792ae87f7
Will Medina <williams.medinaa@gmail.com>
2026-02-07 18:49:02 -0700

    chore: toggle diff view

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,7 +3,7 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-      signs = {
+      signs              = {
         add          = { text = '┃' },
         change       = { text = '┃' },
         delete       = { text = '_' },
@@ -11,17 +11,17 @@ return {
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
       },
-      auto_attach = true,
-      signcolumn = true,
+      auto_attach        = true,
+      signcolumn         = true,
       current_line_blame = true,
-      word_diff  = false,
-      watch_gitdir = {
+      word_diff          = false,
+      watch_gitdir       = {
         follow_files = true
       },
     },
     keys = {
-      { "<leader>gd", "<cmd>Gitsigns diffthis<cr>",     desc = "Diff This" },
-      { "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", desc = "Reset File" },
+      { "<leader>gd", "<cmd>Gitsigns toggle_diff<cr>",  desc = "Diff View" },
+      { "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", desc = "Reset" },
     }
   },
   {
