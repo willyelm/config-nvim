836fef25effc02adb55c8cf6a328e50ec11f4f0b
Will Medina <williams.medinaa@gmail.com>
2026-02-11 08:15:08 -0700

    chore: add alternate focus keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,11 @@
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
 --vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Documentation" })
-vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { noremap = true, silent = true })
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float, {
-  desc = "Show line diagnostics",
-  silent = true
+vim.keymap.set("n", "K", function()
+	vim.lsp.buf.hover()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, {
+	desc = "Show line diagnostics",
+	silent = true,
 })
 -- Save
 vim.keymap.set({ "n", "i" }, "<C-s>", "<Esc>:write<CR>", { desc = "Save Changes" })
@@ -16,5 +18,6 @@ vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Comment selection
 
 -- NvimTree
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit Window" })
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<cr>", { desc = "Focus Explorer" })
+-- vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<cr>", { desc = "Focus Explorer" })
 vim.keymap.set("n", "<leader>\\", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle Explorer" })
+vim.keymap.set("n", "<leader>a", "<C-w>p", { desc = "Alternate Window Focus" })
