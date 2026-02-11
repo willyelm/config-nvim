3578e328d045d399642722901778a02011b80b35
Will Medina <williams.medinaa@gmail.com>
2026-02-11 08:29:50 -0700

    chore: use direction arrows for AI suggestions

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = true,
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            accept      = "<C-y>", -- "Yes" (Vim native confirm)
-            next        = "<C-n>", -- "Down" (Vim native move down)
-            prev        = "<C-p>", -- "Up" (Vim native move up)
-            dismiss     = "<C-e>", -- "Exit/End" (Vim native cancel)
-            accept_word = "<C-l>", -- "Right" (Move right one word)
-            accept_line = "<C-f>", -- "Forward" (Move forward one line)
-          },
-        },
-      })
-    end,
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				panel = {
+					enabled = true,
+				},
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					keymap = {
+						accept = "<C-Right>", -- Accept the full ghost text
+						next = "<C-Down>", -- Cycle to the next suggestion
+						prev = "<C-Up>", -- Cycle to the previous suggestion
+						dismiss = "<C-Left>", -- Keep C-e for "Escape/End"
+						accept_word = "<C-l>",
+						accept_line = "<C-f>",
+					},
+				},
+			})
+		end,
+	},
 }
