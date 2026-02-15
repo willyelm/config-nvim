9e38790be5c441f13f16b51dcc6b3da862ca2e4b
Will Medina <williams.medinaa@gmail.com>
2026-02-15 11:58:04 -0700

    fix: areial config

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,40 +1,40 @@
 return {
-  "stevearc/aerial.nvim",
-  opts = {
-    backends = { "treesitter", "lsp" },
-    on_attach = function(bufnr)
-      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-    end,
-    sep = "",
-    nerd_font = "auto",
-    show_diagnostics = true,
-    highlight_on_hover = true,
-    filter_kind = false,
-    -- Layout settings
-    layout = {
-      default_direction = "left",
-      placement = "window",
-      width = 30,
-    },
-    attach_mode = "global",
-  },
-  keys = {
-    { "<leader>o", "<cmd>AerialToggle!<cr>", desc = "Toggle Outline" },
-  },
-  {
-    "petertriho/nvim-scrollbar",
-    event = "VeryLazy",
-    dependencies = {
-      "lewis6991/gitsigns.nvim",
-    },
-    opts = {
-      show = true,
-      handlers = {
-        gitsigns = true,
-        diagnostic = true,
-        search = true,
-      },
-    },
-  }
+	{
+		"stevearc/aerial.nvim",
+		opts = {
+			backends = { "treesitter", "lsp" },
+			on_attach = function(bufnr)
+				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+			end,
+			sep = "",
+			nerd_font = "auto",
+			show_diagnostics = true,
+			highlight_on_hover = true,
+			filter_kind = false,
+			layout = {
+				default_direction = "left",
+				placement = "window",
+				width = 30,
+			},
+			attach_mode = "global",
+		},
+		keys = {
+			{ "<leader>o", "<cmd>AerialToggle!<cr>", desc = "Toggle Outline" },
+		},
+	},
+	{
+		"petertriho/nvim-scrollbar",
+		event = "VeryLazy",
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+		},
+		opts = {
+			show = true,
+			handlers = {
+				gitsigns = true,
+				diagnostic = true,
+			},
+		},
+	},
 }
