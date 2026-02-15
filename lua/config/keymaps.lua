6921cbf574a68f22afa1a4e2dbc3259d0c785d5f
Will Medina <williams.medinaa@gmail.com>
2026-02-14 23:34:17 -0700

    chore: add heatmap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,20 +3,41 @@ vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename, { desc = "Rename Symbol" }
 
 -- Hovers
 vim.keymap.set("n", "K", function()
-	vim.lsp.buf.hover({
-		border = "rounded",
-		wrap = true,
-		max_width = 60,
-	})
+  vim.lsp.buf.hover({
+    border = "rounded",
+    wrap = true,
+    max_width = 60,
+  })
 end, {
-	noremap = true,
-	desc = "Hover Documentation",
-	silent = true,
+  noremap = true,
+  desc = "Hover Documentation",
+  silent = true,
 })
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, {
-	desc = "Show line diagnostics",
-	silent = true,
+  desc = "Show line diagnostics",
+  silent = true,
 })
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    -- Show diagnostics if there are any on current line
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter" },
+      border = 'rounded',
+      source = 'always',
+    }
+
+    if vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })[1] then
+      vim.diagnostic.open_float(nil, opts)
+    else
+      -- Otherwise show LSP hover
+      vim.lsp.buf.hover()
+    end
+  end,
+})
+
+vim.opt.updatetime = 500
 
 -- Save
 vim.keymap.set({ "n", "i" }, "<C-s>", "<Esc>:write<CR>", { desc = "Save Changes" })
@@ -24,8 +45,8 @@ vim.keymap.set({ "n", "i" }, "<C-s>", "<Esc>:write<CR>", { desc = "Save Changes"
 --vim.keymap.set("n", "<leader>W", "<cmd>wa<cr>", { desc = "Save All Files" })
 
 -- Toggle Comment
-vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Comment line" })
-vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Comment selection" })
+-- vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Comment line" })
+-- vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Comment selection" })
 
 -- NvimTree
 vim.keymap.set("n", "<leader><Tab>", "<C-w>w", { desc = "Next window" })
--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,25 +1,51 @@
 return {
-	"stevearc/aerial.nvim",
-	opts = {
-		backends = { "treesitter", "lsp" },
-		on_attach = function(bufnr)
-			vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-			vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-		end,
-		sep = "",
-		nerd_font = "auto",
-		show_diagnostics = true,
-		highlight_on_hover = true,
-		filter_kind = false,
-		-- Layout settings
-		layout = {
-			default_direction = "left",
-			placement = "window",
-			width = 30,
-		},
-		attach_mode = "global",
-	},
-	keys = {
-		{ "<leader>o", "<cmd>AerialToggle!<cr>", desc = "Toggle Outline" },
-	},
+  "stevearc/aerial.nvim",
+  opts = {
+    backends = { "treesitter", "lsp" },
+    on_attach = function(bufnr)
+      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+    end,
+    sep = "",
+    nerd_font = "auto",
+    show_diagnostics = true,
+    highlight_on_hover = true,
+    filter_kind = false,
+    -- Layout settings
+    layout = {
+      default_direction = "left",
+      placement = "window",
+      width = 30,
+    },
+    attach_mode = "global",
+  },
+  keys = {
+    { "<leader>o", "<cmd>AerialToggle!<cr>", desc = "Toggle Outline" },
+  },
+  {
+    "petertriho/nvim-scrollbar",
+    event = "VeryLazy",
+    dependencies = {
+      "lewis6991/gitsigns.nvim",
+    },
+    opts = {
+      show = true,
+      handle = {
+        color = "#3b4261",
+      },
+      marks = {
+        Search = { color = "#ff9e64" },
+        Error = { color = "#db4b4b" },
+        Warn = { color = "#e0af68" },
+        Info = { color = "#0db9d7" },
+        Hint = { color = "#1abc9c" },
+        Misc = { color = "#9d7cd8" },
+      },
+      handlers = {
+        gitsigns = true,   -- Show git changes
+        diagnostic = true, -- Show diagnostics
+        search = true,     -- Show search results
+      },
+    },
+  }
 }
