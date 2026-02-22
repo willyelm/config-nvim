f119f8b14551c23ba167f3d5c3d4d23a341bab40
Will Medina <williams.medinaa@gmail.com>
2026-02-22 08:59:45 -0700

    fix: disable sidekick

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -23,24 +23,24 @@ return {
 			})
 		end,
 	},
-	{
-		dir = "~/Git/sidekick.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("sidekick").setup({
-				hints = {
-					enabled = true,
-					tip = "Implement with AI (⌥+a)",
-				},
-				tools = {
-					claude = "claude --permission-mode bypassPermissions",
-					ollama = "ollama launch claude",
-				},
-			})
-		end,
-		keys = {
-			{ "<M-a>", "<cmd>Sidekick<cr>", desc = "Start agent tool" },
-		},
-	},
+	-- {
+	-- 	dir = "~/Git/sidekick.nvim",
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	dependencies = { "nvim-lua/plenary.nvim" },
+	-- 	config = function()
+	-- 		require("sidekick").setup({
+	-- 			hints = {
+	-- 				enabled = true
+	-- 				tip = "Implement with AI (⌥+a)",
+	-- 			},
+	-- 			tools = {
+	-- 				claude = "claude --permission-mode bypassPermissions",
+	-- 				ollama = "ollama launch claude",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	keys = {
+	-- 		{ "<M-a>", "<cmd>Sidekick<cr>", desc = "Start agent tool" },
+	-- 	},
+	-- },
 }
